4821937cb8a111a86a668b5f6f265b45a9758992
horrormankind666 <yutthaphoom.taw@mahidol.ac.th>
2022-07-06 13:34:52 +0700

    ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,10 +14,10 @@ export const environment = {
     production: true,
     devYear: 2021,
     lang: 'th',
-    /*
     apiURL: 'https://employment-qas.mahidol.ac.th/Questionnaire/API',
-    */
+    /*
     apiURL: 'https://employment.mahidol.ac.th/Questionnaire/API',
+    */
     isFirstload: false,
     isLoading: false,
     isStartAuthen: false,
@@ -41,25 +41,25 @@ export const environment = {
         scope: 'allatclaims',
         responseType: 'code',
         grantType: 'authorization_code',
-        /*
         redirectURL: 'https://employment-qas.mahidol.ac.th',
-        */
+        /*
         redirectURL: 'https://employment.mahidol.ac.th',
+        */
         authorizationURL: 'https://idp.mahidol.ac.th/adfs/oauth2/authorize',
         tokenURL: 'https://idp.mahidol.ac.th/adfs/oauth2/token',
         logoutURL: 'https://idp.mahidol.ac.th/adfs/oauth2/logout',
     },
     msentConfig: {
         lang: 'TH',
-        /*
         clientID: 'SIT_KEY_EMPLOYMENT',
         clientSecret: '82657b3d-b020-4f52-b447-a367c60a0357',
         consentURL: 'https://privacy-qas.mahidol.ac.th/Msent/Pages/consents.html',
         redirectURL: 'https://employment-qas.mahidol.ac.th/Questionnaire/'
-        */
+        /*
         clientID: 'PRD_KEY_EMPLOYMENT',
         clientSecret: 'fd57d717-cdbd-45b6-bfa6-aa05a5d859e4',
         consentURL: 'https://privacy.mahidol.ac.th/Msent/Pages/consents.html',
         redirectURL: 'https://employment.mahidol.ac.th/Questionnaire/'
+        */
     }
 };
